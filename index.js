6fb304f510f81ba2ada149b8dd94b4844415b2fb
WaveX-j <72532997+WaveX-j@users.noreply.github.com>
2022-08-14 08:50:23 +0500

    Nested fixes

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
 var express = require('express');
-var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
+var {graphqlHTTP} = require('express-graphql');
 var fs = require("fs");
-//var { DataStore } = require('notarealdb');
-const { report } = require('process');
-const { DB } = require('./db');
+const {DB} = require('./db');
 const db = new DB('localhost', 'root', 'root');
+const {makeExecutableSchema} = require('@graphql-tools/schema');
 
-var schema = buildSchema(fs.readFileSync("schema.graphql").toString());
+const typeDefs = fs.readFileSync("schema.graphql").toString()
 
-var root = {
-  listMovies: async () => {
-    try {
-      const result = await db.getMovies()
-      return result
+var resolvers = {
+    Query: {
+        listMovies: async () => {
+            try {
+                const result = await db.getMovies()
+                return result
+            } catch (e) {
+                throw new Error(e)
+            }
+        },
+    },
+
+    Movie: {
+        Actors(movie) {
+            //TODO: Write code here to return actors present in movie
+            console.log('movie', movie)
+            return [{ID: "123", Name: "XYZ", AgentName: "Alex"}];
+        }
     }
-    catch (e) {
-      throw new Error(e)
-    }
-  },
-  
-  Actors: () => {
-      return [{ ID: "123", Name: "XYZ", AgentName: "Alex" }];
-  }
 }
 
-
+// We are using this library to make a schema in which we can inject our nested resolvers
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
+    schema: schema,
+    //rootValue: root,
+    graphiql: true,
 }));
 app.listen(4000);
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
